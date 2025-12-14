ccedc57cf1d3c7dc77136bf7a05c05b006889564
k-redstone <che0618@naver.com>
2025-12-14 19:19:35 +0900

    feat: vercel 종속성 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://saebaldong.kr'],
+    origin: [
+      'http://localhost:3000',
+      'https://saebaldong.kr',
+      'https://saebaldongfe.vercel.app',
+    ],
     credentials: true,
   })
 

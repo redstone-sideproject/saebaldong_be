bf3897ee7ec3b35d1d8fcb92e808a44a4c0a1cfd
k-redstone <che0618@naver.com>
2025-12-14 19:29:53 +0900

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://saebaldong.kr',
-      'https://saebaldongfe.vercel.app',
-    ],
+    origin: true,
     credentials: true,
   })
 

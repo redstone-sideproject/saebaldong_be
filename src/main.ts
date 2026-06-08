d79db7f6f80465deaa6fd8cf02daf6ad5d14d110
k-redstone <che0618@naver.com>
2026-06-09 03:03:26 +0900

    chore: 이슈 해결

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@src/app.module'
-import { ValidationPipe, BadRequestException } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import * as cookieParser from 'cookie-parser'
+import type { Request, Response } from 'express'
 
-async function bootstrap() {
+async function createNestApp() {
   const app = await NestFactory.create(AppModule)
+
   app.enableCors({
     origin: true,
     credentials: true,
@@ -13,16 +15,34 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      // exceptionFactory: (errors) => {
-      //   // 첫 번째 오류 메시지만 반환
-      //   const firstError = Object.values(errors[0].constraints || {})[0]
-      //   return new BadRequestException(firstError)
-      // },
     }),
   )
+
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser())
 
+  return app
+}
+
+// 로컬/일반 서버 실행 (Vercel 서버리스 환경이 아닐 때만 포트 바인딩)
+async function bootstrap() {
+  const app = await createNestApp()
   await app.listen(process.env.PORT ?? 8000)
 }
-bootstrap()
+
+// Vercel 서버리스 핸들러 (warm 인스턴스 간 재사용을 위해 캐싱)
+let cachedServer: ((req: Request, res: Response) => void) | null = null
+
+export default async function handler(req: Request, res: Response) {
+  if (!cachedServer) {
+    const app = await createNestApp()
+    await app.init()
+    cachedServer = app.getHttpAdapter().getInstance()
+  }
+
+  return cachedServer!(req, res)
+}
+
+if (!process.env.VERCEL) {
+  void bootstrap()
+}
